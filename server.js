e744a804622cb0b51c38d9bd72a3cb5348404900
Ryan McConnell <r.j.mcconnell1@rgu.ac.uk>
2018-04-12 09:18:23 +0100

    changes made to display homepage

--- server.js
+++ server.js
@@ -1,12 +1,26 @@
 //server.js
 //load the things
-var express = require('express');
-var app = express();
 
+const MongoClient = require('mongodb').MongoClient;
+const url = "mongodb://localhost:27017/star_wars_quotes";
+const express = require('express');
+const bodyParser = require('body-parser')
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}))
 app.use(express.static('public'))
 
 //set the view engine to express
 app.set('view engine', 'ejs');
+
+var db;
+
+MongoClient.connect(url, function(err, database) {
+  if (err) throw err;
+  db = database;
+  app.listen(8080);
+  console.log('listening');
+});
 
 //use .render to load up an ejs view file
 
