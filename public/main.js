77be8556a1ba38cd1f3f2a409059b10f9dcefa7a
Ryan McConnell <r.j.mcconnell1@rgu.ac.uk>
2018-04-12 14:21:06 +0100

    printing result

--- public/main.js
+++ public/main.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
     // console.log("Made it");
     var cat = event.target.id;
     console.log(cat);
-    var url = "/list?cat="+cat;
+    var url = "/getcategory?cat="+cat;
     console.log(url);
 
     $.ajax({
