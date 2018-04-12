46406c49b6e9dfd161e4501bc69caf1e9a2d10de
Ryan McConnell <r.j.mcconnell1@rgu.ac.uk>
2018-04-12 13:20:43 +0100

    rendering the new page

--- server.js
+++ server.js
@@ -35,7 +35,9 @@ var cat = unescape(req.query.cat);
 console.log(cat);
 db.collection('software').find({category: cat}).toArray(function(err, result) {
   if (err) throw err;
-  res.send(result);
+  res.render('pages/list', {
+    software: result
+  });
 });
 
 });
