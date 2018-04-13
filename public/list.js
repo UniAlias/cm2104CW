80cb088a0c0c6081439f1f4d3f56541ed35afdb8
Ryan McConnell <ryanmcconnell.rm@gmail.com>
2018-04-13 22:56:28 +0100

    changes made so that it displays in proper format.

--- public/list.js
+++ public/list.js
@@ -11,10 +11,12 @@ $(document).ready(function() {
         console.log(result);
         //window.location.assign("/list");
         var divToFill = $('.container');
-        var output = "";
+        var output ="<h2>" + result[0].category + "</h2><div class='panel-group'";
         for (var i in result) {
-          output += "<p>" + result[i].name + "</p>";
+          output += "<div class='panel panel-default'><div class='panel-heading'>" + result[i].name + "</div><div class='panel-body'><article><img class='thumbnail'";
+          output += "src='placeholderImage.png' alt='Placeholder Image'><div class='item-desc'>" + result[i].description + "</div></article></div></div>";
         }
+        output += "</div>";
         divToFill.html(output);
       }
     });
