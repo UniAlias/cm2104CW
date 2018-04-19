3e4d63b5a87bbd8bd7978141d799cb07a12cd148
Ryan McConnell <r.j.mcconnell1@rgu.ac.uk>
2018-04-19 10:32:13 +0100

    changes to make the title appear better

--- public/list.js
+++ public/list.js
@@ -11,7 +11,7 @@ $(document).ready(function() {
         console.log(result);
         //window.location.assign("/list");
         var divToFill = $('.container');
-        var output ="<h2>" + result[0].category + "</h2><div class='panel-group'>";
+        var output ="<h2 class='categoryTitle'>" + result[0].category + "</h2><div class='panel-group'>";
         for (var i in result) {
           output += "<div class='panel panel-default'><div class='panel-heading'>" + result[i].name + "</div><div class='panel-body'><article><img class='thumbnail'";
           output += "src='" + result[i].img_url + ".jpg' alt='Placeholder Image'><div class='item-desc'>" + result[i].description + "</div></article></div></div>";
