2d24a018d129ed0a463105ac4beba130a1259ea0
Ryan McConnell <r.j.mcconnell1@rgu.ac.uk>
2018-04-20 16:25:13 +0100

    update to the linking.

--- public/list.js
+++ public/list.js
@@ -13,8 +13,8 @@ $(document).ready(function() {
         var divToFill = $('.container');
         var output ="<h2 class='categoryTitle'>" + result[0].category + "</h2><div class='panel-group'>";
         for (var i in result) {
-          output += "<a href='" + result[i].url_link + "'><div class='panel panel-default'><div class='panel-heading'>" + result[i].name + "</div><div class='panel-body'><article><img class='thumbnail'";
-          output += "src='" + result[i].img_url + ".jpg' alt='Placeholder Image'><div class='item-desc'>" + result[i].description + "</div></article></div></a></div>";
+          output += "<div class='panel panel-default'><div class='panel-heading'>" + result[i].name + "</div><div class='panel-body'><article><img class='thumbnail'";
+          output += "src='" + result[i].img_url + ".jpg' alt='Placeholder Image'><div class='item-desc'>" + result[i].description + "<a href='" + result[i].url_link + "'>Download here.</a></div></article></div></div>";
         }
         output += "</div>";
         divToFill.html(output);
